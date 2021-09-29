a8a0b6c1eec0abeef0342b14f88899c7f945ae77
Asadbek07 <asindarov07@gmail.com>
2021-09-29 22:29:05 +0500

    update file type

--- files/models.py
+++ files/models.py
@@ -2,7 +2,26 @@ from django.db import models
 import os
 from django.template.defaultfilters import slugify
 from django.urls import reverse
+
 # Create your models here.
+
+
+WORD = (
+	".docx",".doc", ".docm", ".dotx", ".dotm", ".odt"
+	)
+
+EXCEL = (
+	".xlsx", ".xlsm", ".xltx", ".xltm", ".xlsb", ".xlam",
+	)
+
+POWER_POINT = (
+	".pptx", ".pptm", ".potx", ".potm", ".ppam", 
+	".ppsx", ".ppsm", ".sldx", ".sldm", ".thmx",
+	)
+
+IMAGE_TYPES = (".jpeg", ".png", ".jpg", )
+
+PDF = (".pdf")
 
 
 class Directory(models.Model):
@@ -10,7 +29,6 @@ class Directory(models.Model):
 	directories = models.ForeignKey('self', on_delete=models.SET_NULL, related_name="directories_of_this", null=True, blank=True)
 	created = models.DateTimeField(auto_now_add=True)
 	slug = models.SlugField(max_length=255, unique=True, null=True, blank=True)
-
 	def save(self, *args, **kwargs):
 		self.slug = slugify(self.title)
 		super(Directory, self).save(*args, **kwargs)
@@ -32,6 +50,8 @@ class File(models.Model):
 	directory = models.ForeignKey('Directory', on_delete=models.SET_NULL, related_name="files_set", null=True, blank=True)
 	slug = models.SlugField(max_length=255, null=True, blank=True, unique=True)
 	created = models.DateTimeField(auto_now_add=True)
+	file_type = models.CharField(max_length=100, null=True, blank=True)
+	
 	def extension(self):
 		name, extension = os.path.splitext(self.file.name)
 		# print(name, extension)
@@ -39,6 +59,20 @@ class File(models.Model):
 
 	def save(self, *args, **kwargs):
 		self.slug = slugify(self.title)
+		print(self.extension()[1])
+		print(self.directory)
+		if self.extension()[1] in WORD:
+			self.file_type = "word"
+		elif self.extension()[1] in EXCEL:
+			self.file_type = "excel"
+		elif self.extension()[1] in POWER_POINT:
+			self.file_type = "power_point"
+		elif self.extension()[1] in IMAGE_TYPES:
+			self.file_type = "image"
+		elif self.extension()[1] in PDF:
+			self.file_type = "pdf"		
+		else:
+			self.file_type = "undefined"			
 		super(File, self).save(*args, **kwargs)
 
 	
