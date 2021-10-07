4b27dd530b2343d665821c70cccb54977c2ee061
Asadbek07 <asindarov07@gmail.com>
2021-10-07 11:11:51 +0500

    update forms

--- files/urls.py
+++ files/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name = "files"
 
 urlpatterns = [
-	path("", views.list_files, name="files-list"),
+	path("", views.CreateDirectoryView.as_view(), name="files-list"),
 	# path("file/create/", views.list_files, name="file-create"),
 	path("directory/create/", views.CreateDirectoryView.as_view(), name="directory-create"),
 	path("search/", views.search, name='search'),
 	# path("<uuid:uuid_id>/", views.testview, name="test"),
 	path("detail/<uuid:uuid>/", views.DetailFileView.as_view(), name="file-detail"),
-	path("<uuid:uuid>/", views.list_files, name="files-list-detail"),
+	path("<uuid:uuid>/", views.CreateDirectoryView.as_view(), name="files-list-detail"),
 
 ]
--- files/views.py
+++ files/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from .models import File, Directory, Department
 from django.views import generic
 from .forms import CreateDirectoryForm
 
 
-def list_files(request, uuid=None):
-	""" List View for displaying all the files and directories """
-	if uuid:
-		directory = get_object_or_404(Directory, uuid_id=uuid)
-		directory_objects = directory.directories_of_this.all()
-		file_objects = directory.files_set.all()
-	else:	
-		directory_objects = Directory.objects.filter(directories=None)
-		file_objects = File.objects.filter(directory=None)
-	directory_create_form = CreateDirectoryForm() 	
-	# print(directory_objects)
-	return render(request, "file/index.html", {"directory_form" : directory_create_form  , "directory_objects": directory_objects, "file_objects": file_objects})
+# def list_files(request, uuid=None):
+# 	""" List View for displaying all the files and directories """
+# 	if uuid:
+# 		directory = get_object_or_404(Directory, uuid_id=uuid)
+# 		directory_objects = directory.directories_of_this.all()
+# 		file_objects = directory.files_set.all()
+# 	else:	
+# 		directory_objects = Directory.objects.filter(directories=None)
+# 		file_objects = File.objects.filter(directory=None)
+# 	directory_create_form = CreateDirectoryForm() 	
+# 	# print(directory_objects)
+# 	return render(request, "file/index.html", {"directory_form" : directory_create_form  , "directory_objects": directory_objects, "file_objects": file_objects})
 
 
 
@@ -29,14 +29,41 @@ class DetailFileView(generic.DetailView):
 		obj = get_object_or_404(File, uuid_id=self.kwargs['uuid'])
 		return obj
 			
-class CreateDirectoryView(generic.FormView):
+class CreateDirectoryView(generic.View):
 	template_name = "file/index.html"
 	form_class = CreateDirectoryForm
 	success_url = '/'
+	def get(self, request, uuid=None, *args, **kwargs):
+		if uuid:
+			directory = get_object_or_404(Directory, uuid_id=uuid)
+			directory_objects = directory.directories_of_this.all()
+			file_objects = directory.files_set.all()
+		else:	
+			directory_objects = Directory.objects.filter(directories=None)
+			file_objects = File.objects.filter(directory=None)
+		directory_create_form = CreateDirectoryForm() 	
+		# print(directory_objects)
+		return render(request, "file/index.html", {"directory_form" : directory_create_form  , "directory_objects": directory_objects, "file_objects": file_objects})
+ 
+	def post(self, request, uuid=None, *args, **kwargs):
+		directory_parent = None
+		if uuid:
+			directory_parent = get_object_or_404(Directory, uuid_id=uuid)
+		
+		author = request.user
+		form = CreateDirectoryForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			if directory_parent:
+				directory_child = Directory.objects.create(author=author, title=cd.get('title'), directories=directory_parent) 	
+			else:
+				directory_child = Directory.objects.create(author=author, title=cd.get('title')) 	
 
-# def directory_create_form(request, slug=False)
-
-
+			directory_child.save()
+		if directory_parent:
+			return redirect(directory_parent.get_absolute_url())	
+		return redirect('/')	
+			
 # Search form
 def search(request):
 	if request.method == "POST":
